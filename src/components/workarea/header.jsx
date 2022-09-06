205fc760a3b0ae02e76a514b4d314e9db42f8d86
Praisecodes <cmancoder@gmail.com>
2022-09-06 19:18:43 +0100

    [Update] Working on Header (NEPA)

--- src/components/workarea/header.jsx
+++ src/components/workarea/header.jsx
@@ -1,8 +1,18 @@
+import {FaBell, FaMoon} from "react-icons/fa"
+
 function Header(){
     return (
         <>
         <div className="header">
-            
+            <form onSubmit={(e)=>{e.preventDefault()}}>
+                <input type="search" placeholder="Activity Title" />
+                <button type="submit">Search</button>
+            </form>
+            <div className="header_menu">
+                <i><FaMoon /></i>
+                <i><FaBell /></i>
+                <p>Christian</p>
+            </div>
         </div>
         </>
     )
