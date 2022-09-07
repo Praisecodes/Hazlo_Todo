249e0ada101f9ed01e265a5dfe0496931a419912
Praisecodes <cmancoder@gmail.com>
2022-09-07 23:55:20 +0100

    [Update] Added section select

--- src/components/workarea/sidenav.jsx
+++ src/components/workarea/sidenav.jsx
@@ -3,8 +3,11 @@ import {FaHome, FaChartBar, FaArchive, FaTrash, FaStar, FaCheckCircle, FaArrowsA
 
 function SideNav(){
 
-    const SwitchUp = (events, ele1, ele2, ele3, ele4) =>{
+    const SwitchUp = (events, ...args) =>{
         events.target.classList.add("selected")
+        args.forEach((arg)=>{
+            document.querySelector(`.${arg}`).classList.remove("selected")
+        })
     }
     
     return (
@@ -14,27 +17,27 @@ function SideNav(){
                 <img src={Logo} alt="" />
                 <h2>Hazlo Todo</h2>
             </div> <br /><br />
-            <p className="sections selected" onClick={SwitchUp}>
+            <p className="sections dashOption selected" onClick={(e)=>{SwitchUp(e, "activityOption", "archiveOption", "trashOption", "starOption", "completeOption")}}>
                 <i><FaHome/></i>
                 Dashboard
             </p>
-            <p className="sections" onClick={SwitchUp}>
+            <p className="sections activityOption" onClick={(e)=>{SwitchUp(e, "dashOption", "archiveOption", "trashOption", "starOption", "completeOption")}}>
                 <i><FaChartBar/></i>
                 Activities
             </p>
-            <p className="sections" onClick={SwitchUp}>
+            <p className="sections archiveOption" onClick={(e)=>{SwitchUp(e, "dashOption", "activityOption", "trashOption", "starOption", "completeOption")}}>
                 <i><FaArchive/></i>
                 Archives
             </p>
-            <p className="sections">
+            <p className="sections trashOption" onClick={(e)=>{SwitchUp(e, "dashOption", "archiveOption", "activityOption", "starOption", "completeOption")}}>
                 <i><FaTrash /></i>
                 Trash Bin
             </p>
-            <p className="sections">
+            <p className="sections starOption" onClick={(e)=>{SwitchUp(e, "dashOption", "archiveOption", "trashOption", "activityOption", "completeOption")}}>
                 <i><FaStar /></i>
                 Starred
             </p>
-            <p className="sections">
+            <p className="sections completeOption" onClick={(e)=>{SwitchUp(e, "dashOption", "archiveOption", "trashOption", "starOption", "activityOption")}}>
                 <i><FaCheckCircle/></i>
                 Completed
             </p>
