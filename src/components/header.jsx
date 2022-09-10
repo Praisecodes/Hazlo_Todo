f349ea06d5d7a954c7d6adbcc5deac9b7c7d7ab6
Praisecodes <cmancoder@gmail.com>
2022-09-10 20:16:05 +0100

    [Update] Update Header Component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,5 @@
+import { FaAngleDown, FaBell, FaMoon } from "react-icons/fa"
+
 export default function Header(){
     return (
         <>
@@ -6,6 +8,23 @@ export default function Header(){
                 <input type="search" placeholder="Enter Activity Title" />
                 <button type="submit">Search</button>
             </form>
+            <div className="rightDiv">
+                <i>
+                    <FaMoon />
+                </i>
+                <i>
+                    <FaBell />
+                </i>
+                <p>Christian</p>
+                <div className="imageBox">
+                    <div className="circle">
+
+                    </div>
+                    <i>
+                        <FaAngleDown />
+                    </i>
+                </div>
+            </div>
         </div>
         </>
     )
