d6ef9c0ca9ad08122e1a35348e66c0f70e6c65a0
Praisecodes <cmancoder@gmail.com>
2022-09-19 19:37:52 +0100

    [Update] Complete Dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,6 +2,13 @@ import { Helmet } from "react-helmet"
 import SideNav from "../components/sidenav"
 import Header from "../components/header"
 import DashImage from "../assets/dash_image.svg"
+import TotalImage from "../assets/total_image.png"
+import DueImage from "../assets/due_image.png"
+import CompleteImage from "../assets/complete_image.png"
+import ArchivedImage from "../assets/archived_image.png"
+import UnfinishedImage from "../assets/unfinished_image.png"
+import TrashedImage from "../assets/trashed_image.png"
+import StarredImage from "../assets/starred_image.png"
 import { Link } from "react-router-dom"
 import { FaPlus } from "react-icons/fa"
 
@@ -40,29 +47,85 @@ export default function Dashboard(){
                             </i> <br />
                             <p>Create New Schedule</p>
                         </div>
-                    </div>
+                    </div> <br />
 
                     <div className="counts">
                         <div className="cards">
-
+                            <div className="imageDiv blue">
+                                <img src={TotalImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    12
+                                </h1>
+                                <p>Total Activities</p>
+                            </div>
                         </div>
                         <div className="cards">
-                            
+                            <div className="imageDiv yellow">
+                                <img src={DueImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    0
+                                </h1>
+                                <p>Activities Due Today</p>
+                            </div>
                         </div>
                         <div className="cards">
-                            
+                            <div className="imageDiv completePink">
+                                <img src={CompleteImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    4
+                                </h1>
+                                <p>Activities Completed</p>
+                            </div>
                         </div>
                         <div className="cards">
-                            
+                            <div className="imageDiv dashGray">
+                                <img src={ArchivedImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    10
+                                </h1>
+                                <p>Archived Activities</p>
+                            </div>
                         </div>
                         <div className="cards">
-                            
+                            <div className="imageDiv dashGreen">
+                                <img src={UnfinishedImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    2
+                                </h1>
+                                <p>Unfinished Activities</p>
+                            </div>
                         </div>
                         <div className="cards">
-                            
+                            <div className="imageDiv dashPurple">
+                                <img src={TrashedImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    1
+                                </h1>
+                                <p>Trashed Activities</p>
+                            </div>
                         </div>
                         <div className="cards">
-                            
+                            <div className="imageDiv darkerBlue">
+                                <img src={StarredImage} alt="" />
+                            </div>
+                            <div className="text">
+                                <h1>
+                                    6
+                                </h1>
+                                <p>Starred Activities</p>
+                            </div>
                         </div>
                     </div>
                 </div>
