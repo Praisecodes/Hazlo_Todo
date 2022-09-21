ffb55e87eb290640afee3e0d66d5014347480761
Praisecodes <cmancoder@gmail.com>
2022-09-21 20:19:10 +0100

    [Update] Added Login Feature

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,10 +7,48 @@ function Login(){
     let nav = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [jwt, setJwt] = useState("");
 
     function vaidateLogin(event){
         event.preventDefault();
-        localStorage.setItem('username', username);
+        
+        fetch("http://localhost/hazloapi/login.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        })
+        .then((res)=>{
+            let status = res.status;
+            switch(status){
+                case 200:
+                    localStorage.setItem('username', username);
+                    return res.json();
+                    break;
+                case 403:
+                    console.log("Unauthorized");
+                    break;
+                case 404:
+                    console.log("No User Found");
+                    break;
+                case 500:
+                    console.log(res.statusText);
+                    break;
+                default:
+                    console.log("No Input");
+            }
+        })
+        .then((data)=>{
+            if(data){
+                localStorage.setItem('token', data);
+                nav("/dashboard");
+            }
+        })
+        .catch((err)=>{console.log(err)});
     }
 
     function toSignUp(){
