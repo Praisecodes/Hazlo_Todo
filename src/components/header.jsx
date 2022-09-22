33f84ed08ee8c29180cad247cbba3eae20e4a169
Praisecodes <cmancoder@gmail.com>
2022-09-22 12:26:02 +0100

    [Update] Added Logout feature

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,16 @@
-import { FaAngleDown, FaBell, FaMoon } from "react-icons/fa"
+import { FaAngleDown, FaArrowLeft, FaArrowRight, FaBell, FaMoon } from "react-icons/fa"
 import { useState, useTransition } from "react"
+import { useNavigate } from "react-router-dom";
 
 export default function Header(){
     const [username, setUsername] = useState(localStorage.getItem('username'));
+    const [dropDown, setDropDown] = useState(false);
+    const nav = useNavigate();
+    function handleLogout(){
+        localStorage.removeItem('username'); 
+        localStorage.removeItem('token'); 
+        nav('/login');
+    }
     return (
         <>
         <div className="header">
@@ -18,13 +26,21 @@ export default function Header(){
                     <FaBell />
                 </i>
                 <p>{username}</p>
-                <div className="imageBox">
+                <div className="imageBox" onClick={()=>{(dropDown)?setDropDown(false):setDropDown(true)}}>
                     <div className="circle">
 
                     </div>
                     <i>
                         <FaAngleDown />
                     </i>
+                    <div className={(dropDown)? 'dropdown openByHeight' : 'dropdown closeByHeight'}>
+                        <p onClick={handleLogout}>
+                            Logout
+                            <i>
+                                <FaArrowRight />
+                            </i>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
